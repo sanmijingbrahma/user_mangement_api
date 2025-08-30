c262589b16e0c785d821401a70e64c2c9c40615d
sanmijingbrahma <bsanmijing@gmail.com>
2025-08-30 17:22:10 +0530

    Add Login endpoint with jwt

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const jwt= require("jsonwebtoken");
 
 const pool = require("./db");
 
 const PORT = process.env.PORT || 8000;
+const JWT_SECRET= process.env.JWT_SECRET;
 
 
 app.use(express.json());
@@ -29,15 +31,44 @@ app.post("/api/register", async (req,res)=>{
         const user = await pool.query("INSERT INTO users(name,email,password_hash)VALUES($1,$2,$3) RETURNING id,name,email",[name,email,hashPassword]);
         res.json(user.rows[0]);
 
+    }catch(err){
+        console.error(err.message);
+        res.status(500).json({"message":"Server Error"})
+
+    }
+})
+
+
+app.post("/api/login", async (req,res)=>{
+
+    try{
+        const {email,password} = req.body;
+        
+        const result = await pool.query("SELECT * FROM users where email=$1",[email]);
+        const user = result.rows[0];
+        if(!user){
+            res.status(400).josn({error:"invalid credentials"})
+        }
+        const match = await bcrypt.compare(password,user.hashPassword);
+        if(!match){
+            res.status(400).json({error:"invalid credentials"});
+        }
+        
+        const token = jwt.sign(
+            {id:user.id,email:user.email},
+            JWT_SECRET,
+            {expiresIn:"15m"},
+        )
+
+        res.json({token});
 
     }catch(err){
         console.error(err.message);
         res.status(500).json({"message":"Server Error"})
 
     }
-
-
 })
+
 
 
 app.listen(PORT,()=>{
