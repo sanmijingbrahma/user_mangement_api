c6871e6126e4f9a9b6bb6f1c3a526b03df37cf89
sanmijingbrahma <bsanmijing@gmail.com>
2025-09-01 22:45:51 +0530

    Refactor with DB string

--- db.js
+++ db.js
@@ -2,12 +2,8 @@ require("dotenv").config();
 const {Pool} = require("pg");
 
 const pool = new Pool({
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    user:process.env.DB_USER,
-    database:process.env.DB_DATABASE,
-    password:process.env.DB_PASSWORD,
-
+    connectionString:process.env.DB_URL,
+    ssl:{rejectUnauthorized:false},
 })
 
 
