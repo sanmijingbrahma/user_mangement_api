a4afc36179db86eade428a25f450b92555cf00b6
sanmijingbrahma <bsanmijing@gmail.com>
2025-09-01 22:46:14 +0530

    Automate DB initalization

---
+++ initDb.js
@@ -0,0 +1,21 @@
+const pool = require("./db");
+
+async function init() {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name TEXT NOT NULL,
+      email TEXT UNIQUE NOT NULL,
+      password TEXT NOT NULL
+    );
+  `);
+
+  console.log("Database initialized");
+  process.exit(0);
+}
+
+init().catch(err => {
+  console.error("DB initialization failed:", err);
+  process.exit(1);
+});
+
