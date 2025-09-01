b06f3e307534f5ff228ccd2da610215e08bd343b
sanmijingbrahma <bsanmijing@gmail.com>
2025-09-01 22:55:49 +0530

    Fix initDb.js mistake

--- initDb.js
+++ initDb.js
@@ -6,7 +6,7 @@ async function init() {
       id SERIAL PRIMARY KEY,
       name TEXT NOT NULL,
       email TEXT UNIQUE NOT NULL,
-      password TEXT NOT NULL
+      password_hash TEXT NOT NULL
     );
   `);
 
